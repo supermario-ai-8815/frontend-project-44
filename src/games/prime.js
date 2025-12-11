1a21417797c3b403747b778fc71c2fa71dd884d2
Ivan Zhirkov <iazhirkov2002@yandex.ru>
2025-12-11 19:31:43 +0300

    step 9, gebug Prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,27 +1,35 @@
 
 export function isPrime () {
-  //let result = ((num % 1 === 0) && (num % num === 0)) ? true : false
   let num = Math.floor(Math.random() * 100) + 1
-  if ((num % 2 === 0) || (num < 2)) {
+  if (num <= 1) return {
+    correctResult: 'no',
+    question: `${num}`
+  } // 0, 1 и отрицательные не простые
 
+  if (num <= 3) {
+    return {
+    correctResult: 'yes',
+    question: `${num}`  // 2 и 3 простые
+  }}
+  // Исключаем четные числа и кратные 3
+  if (num % 2 === 0 || num % 3 === 0) {
     return {
     correctResult: 'no',
     question: `${num}`
-   //console.log(`${num} - no`)
-    }
-  } else if ((num === 2) || ((num % 2 !== 0) && (num < Math.sqrt(num)))) {
-    //console.log(`${num} - yes`)
-    return {
-        correctResult: 'yes',
+  }}
+  
+  // Проверяем делители от 5 до √num
+  // Используем шаг 6 (оптимизация)
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return {
+        correctResult: 'no',
         question: `${num}`
-    }
-  } else {
-    //console.log(`${num} - yes`)
-    return {
-        correctResult: 'yes',
-        question: `${num}`
-    }
-  }
+  }};
+  
+  return true;
+}
 }
 
 //isPrime()
+
