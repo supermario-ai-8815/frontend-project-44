84fcb34c4186cbfeeef578c5271d54f996bf0bbe
Ivan Zhirkov <iazhirkov2002@yandex.ru>
2025-12-15 18:30:10 +0300

    debug 4.0

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export const indexDriver = (game) => {
         //console.log('DEBUG: correctResult =', correctResult)
         //console.log('DEBUG: typeof questionNum =', typeof questionNum)
 
-        console.log(`Question: ${questionNum}`)
+        console.log('Question: ' + questionNum)
         const answer = readlineSync.question(`Your answer: `)
         if (answer === correctResult) {
             console.log('Correct!')
